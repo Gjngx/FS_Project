1d0520fb472c4afb62d7f2e88cd4902fcf5c1637
Gjngx <giangdang2k2@gmail.com>
2023-03-20 19:12:10 +0700

    G-P-P

---
+++ FS_Project/Controllers/TimKiemController.cs
@@ -0,0 +1,31 @@
+﻿using FS_Project.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FS_Project.Controllers
+{
+    public class TimKiemController : Controller
+    {
+        // GET: TimKiem
+        public ActionResult Index()
+        {
+            ViewBag.truyenmoi = new TruyenDAO().truyentheosoluong(10);
+            return View();
+        }
+
+        public ActionResult timtacgia() 
+        {
+            ViewBag.truyenmoi = new TruyenDAO().truyentheosoluong(10);
+            return View();
+        }
+
+        public ActionResult timtheloai()
+        {
+            ViewBag.truyenmoi = new TruyenDAO().truyentheosoluong(10);
+            return View();
+        }
+    }
+}
